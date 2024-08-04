32eede9803fdad04856a0c58f684769dba1778ad
Pravahan jaivili <pravahanjaivili@gmail.com>
2024-08-04 15:46:12 +0530

    added git ignore

--- DemoApplication/obj/Debug/net8.0/DemoApplication.AssemblyInfo.cs
+++ DemoApplication/obj/Debug/net8.0/DemoApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993bebd9eaebe30a87b97696fd3a16cc53e962f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
